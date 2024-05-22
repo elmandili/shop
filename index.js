09a314d097edeeeee8c171c735f155ec77933073
elmandili <elmandilimail@gmail.com>
2024-05-22 16:23:41 +0100

    document

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ app.get('/api/find/:id', async(req,res)=>{
 app.get('/api/find-user/:username', async(req, res)=>{
   try
   {
-    const document = await registeredUsers.findOne({"email" : req.params.username}).toArray();
+    const document = await registeredUsers.findOne({"email" : req.params.username});
     console.log(document);
     await res.send(document);
   }
